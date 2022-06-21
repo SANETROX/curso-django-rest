276eec24bf11876df278204ed014b21d969f1676
sanetrox <doom102010.ccsa@gmail.com>
2022-06-20 19:23:30 -0500

    Create UserProfile

---
+++ profiles_api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from profiles_api.models import UserProfile
+# Register your models here.
+admin.site.register(UserProfile)
---
+++ profiles_api/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+class UserProfileManager(BaseUserManager):
+    """
+    Manager para perfiles de User
+    """
+    def create_user(self, email, name, password=None):
+        """
+        Crear User Profile
+        """
+        if not email:
+            raise ValueError('Usuario debe tener un email')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, name,  password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+        Modelo para el User
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name + " " + self.email
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
